6625409ee9b0aefa5781af189deea90d064f4b5a
Junyeong Lee <joonyung2@gmail.com>
2023-02-15 17:30:09 +0900

    Initial Commit

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by JY on 2023/01/05.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true,
+                completion: nil)
+    }
+
+}
